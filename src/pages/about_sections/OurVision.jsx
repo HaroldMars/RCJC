b42eccb5b7263b7bcc125aa233cfe90d89f91df1
HaroldMars <abejar199@gmail.com>
2025-09-28 14:48:44 +0800

    text-fixed

--- src/pages/about_sections/OurVision.jsx
+++ src/pages/about_sections/OurVision.jsx
@@ -4,8 +4,11 @@ function OurVision() {
   return (
     <div>
       <h1 className='font-bold text-blue-700'>RCJCIM Vision Statement</h1>
+      <br />
       <p className='text-blue-400'><b>1.</b> Each one brings one to Jesus.</p>
+      <br />
       <p className='text-blue-400'><b>2.</b> Establish house fellowship in all locations.</p>
+      <br />
       <p className='text-blue-400'><b>3.</b> Members must always obey the words of God.</p>
     </div>
   )
